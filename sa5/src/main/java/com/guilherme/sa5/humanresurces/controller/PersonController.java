fe62ce3d4461d7a104b08a6b76b67b80791e2479
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-05-04 20:51:31 -0300

    SA5 pt 5

--- sa5/src/main/java/com/guilherme/sa5/humanresurces/controller/PersonController.java
+++ sa5/src/main/java/com/guilherme/sa5/humanresurces/controller/PersonController.java
@@ -1,9 +1,11 @@
 package com.guilherme.sa5.humanresurces.controller;
 
+import com.guilherme.sa5.humanresurces.entity.Person;
 import com.guilherme.sa5.humanresurces.reposiroty.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class PersonController {
@@ -18,5 +20,10 @@ public class PersonController {
     public String personList(Model model){
         model.addAttribute("personList", personRepository.findAll());
         return "rh/person/index";
+    }
+
+    @GetMapping("/rh/person/new")
+    public String newPerson(@ModelAttribute("person")Person person){
+        return "rh/person/form";
     }
 }
